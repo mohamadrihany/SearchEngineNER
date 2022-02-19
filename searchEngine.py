3ff9dfbcc39914b85e1f5c19a01e5ac0c4023504
mohamadrihany <mohammad.rihany92@gmail.com>
2022-02-19 18:26:33 +0100

    Add files via upload

---
+++ searchEngine.py
@@ -0,0 +1,45 @@
+import spacy
+import pandas as pd
+from tqdm import tqdm
+import csv
+pd.set_option('display.max_colwidth', None)
+from rank_bm25 import BM25Okapi
+import numpy as np
+from IPython.core.display import display, HTML
+import time
+import json
+import sys
+
+
+
+if __name__ == "__main__":
+    query = sys.argv[1]
+    #print(query)
+    nlp = spacy.load("fr_core_news_lg")
+    df = pd.read_csv('lessurligneurs.csv')
+    tok_text=[] 
+    for doc in tqdm(nlp.pipe(df.NER.str.lower().values, disable=["tagger", "parser","ner"])):
+        tok = [t.text for t in doc if t.is_alpha]
+        tok_text.append(tok)
+        
+    bm25 = BM25Okapi(tok_text)
+
+    #query = "Convention France macron"
+    tokenized_query = query.lower().split(" ")
+
+
+    t0 = time.time()
+    results = bm25.get_top_n(tokenized_query, df.NER.values, n=10)
+    t1 = time.time()
+    print(f'Searched 767 records in {round(t1-t0,3) } seconds \n')
+    for i in results:
+        index = np.where(df.NER.values==i)
+        idItem = df.idItem.values[index]
+        title = df.title.values[index]
+        guid = df.guid.values[index]
+        print("Surlignage id is: ", idItem)
+        print(guid[0])
+        print(title[0])
+        #createstring = "<a href=" + guid[0] + ">" + title[0] + "</a>"
+        #display(HTML(createstring))
+        print('\n------------------------------------------------------------------------------------------------------\n------------------------------------------------------------------------------------------------------')
